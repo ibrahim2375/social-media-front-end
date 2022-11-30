f1494459824f94155a8871d0906cd02643be62b8
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-11-30 03:29:13 +0200

    adding redux

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,45 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+//:redux persistor toolkit/////////////////////////////
+import reducers from './redux/index'
+import {Provider} from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+let Persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+     <Provider store={store}>
+      <PersistGate loading={null} persistor={Persistor} >
     <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 )
