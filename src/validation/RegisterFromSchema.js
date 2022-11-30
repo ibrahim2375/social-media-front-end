80e5363d7812af171254fa54d590608b67aa2a01
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-11-30 03:32:16 +0200

    validation

---
+++ src/validation/RegisterFromSchema.js
@@ -0,0 +1,11 @@
+import * as yup from 'yup'
+
+export const RegisterFormSchema = yup.object().shape({
+    firstname: yup.string().required("required"),
+    lastname: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
+    location: yup.string().required("required"),
+    occupation: yup.string().required("required"),
+    picture: yup.string().required("required"),
+});
